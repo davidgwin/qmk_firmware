5736c48b20bbef0ede5e3bdbc5dcbb1e1c071c20
David Gwin <davidgwin@ymail.com>
2022-02-07 22:44:43 -0700

    adds dynamic macros

--- keyboards/handwired/dactyl_promicro_68/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_promicro_68/keymaps/default/keymap.c
@@ -22,7 +22,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_6x6(
         KC_GRAVE, KC_1 ,KC_2   , KC_3 , KC_4  , KC_5  ,                                        KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
-        KC_TAB , KC_Q  ,KC_W   , KC_E , KC_R  , KC_T  ,                                        KC_Y   , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL,
+        KC_TAB , KC_Q  ,KC_W   , KC_E , KC_R  , KC_T  ,                                        KC_Y   , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL ,
         RG_CAPS, KC_A  ,KC_S   , KC_D , KC_F  , KC_G  ,                                        KC_H   , KC_J  , KC_K  , KC_L  ,KC_SCLN,LG_QUOT,
         KC_LSFT, KC_Z  ,KC_X   , KC_C , KC_V  , KC_B  ,                                        KC_N   , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
         KC_LCTL,KC_LGUI,KC_LALT,KC_ESC ,KC_DEL,KC_BSPC,CTL_SPC, MONA ,      KC_ENT , CTR_SPC, KC_TAB, KC_LEFT,KC_DOWN, KC_UP , KC_RGHT,KC_RCTL,                                                                       
@@ -47,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                               
     ),
     [_LEVAN] = LAYOUT_6x6(
-        _______,_______,_______,_______,_______,_______,                                       _______,_______,KC_PSLS,KC_PAST,KC_PMNS,_______,
-        _______,_______,_______,_______,_______,_______,                                       _______,KC_P7  ,KC_P8  ,KC_P9  ,KC_PPLS,_______,
+        DM_RSTP,DM_PLY1,DM_PLY2,_______,_______,_______,                                       _______,_______,KC_PSLS,KC_PAST,KC_PMNS,_______,
+        DM_RSTP,DM_REC1,DM_REC2,_______,_______,_______,                                       _______,KC_P7  ,KC_P8  ,KC_P9  ,KC_PPLS,_______,
         _______,_______,_______,_______,_______,_______,                                       _______,KC_P4  ,KC_P5  ,KC_P6  ,KC_PCMM,_______,
         _______,_______,_______,_______,_______,_______,                                       _______,KC_P1  ,KC_P2  ,KC_P3  ,KC_PEQL,_______,
-        _______,_______,_______,_______,_______,_______,_______,_______,        _______,_______,_______,_______,KC_P0  ,KC_DOT ,KC_PENT,_______, 
+        _______,_______,_______,_______,_______,_______,_______,_______,        _______,_______,_______,_______,KC_P0 ,KC_DOT ,KC_PENT,_______, 
                                                         _______,_______,        _______,_______
                                                                                
     ) 
