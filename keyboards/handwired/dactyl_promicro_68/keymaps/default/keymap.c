6fe2f90d174a7150f5689db9d620d225c1a411e9
David Gwin <davidgwin@ymail.com>
2022-02-09 20:11:09 -0700

    fixes errrors in last commit

--- keyboards/handwired/dactyl_promicro_68/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_promicro_68/keymaps/default/keymap.c
@@ -18,9 +18,7 @@
 #define CTL_ESC LCTL_T(KC_ESC)
 #define CTL_SPC LCTL_T(KC_SPC)
 #define CTR_SPC RCTL_T(KC_SPC)
-#define LVN_ESC LT(_LEVAN, KC_ESC)
-#define LVN_ESC LT(_LEVAN, KC_Z)
-#define LVN_ESC LT(_LEVAN, KC_ESC)
+#define LVN_ESC LT(_LEVAN, KC_ESC) 
 #define DBEAVER LT(_BEAVER, KC_D)
 #define KKANAB LT(_KANAB, KC_K)
 #define ALT_RGT RALT(KC_RGHT)
@@ -95,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRAVE, KC_1 ,KC_2   , KC_3 , KC_4  , KC_5  ,                                        KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
         KC_TAB , KC_Q  ,KC_W   , KC_E , KC_R  , KC_T  ,                                        KC_Y   , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL ,
         KC_CAPS, A_SHFT,KC_S   ,DBEAVER, KC_F , KC_G  ,                                        KC_H   , KC_J  ,KKANAB , KC_L  ,CL_SHFT,KC_QUOT,
-        KC_LSFT, KC_Z  ,KC_X   , KC_C , KC_V  ,KC_B,                                           KC_N   , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_SFENT,
+        KC_LSFT, KC_Z  ,KC_X   , KC_C , KC_V  ,KC_B,                                           KC_N   , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_SFTENT,
         KC_LCTL,KC_LGUI,KC_LALT,KC_ESC ,KC_DEL,SALEM, KC_BSPC,KC_LCTL,       KC_RALT, KC_SPC, KC_RSFT, KC_LEFT,KC_DOWN, KC_UP , KC_RGHT,  KC_RCTL,                                                                       
                                                         MONA ,LEVAN ,        KC_BSPC, KC_ENT
                                                                             
@@ -104,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F12 ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                                       KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
         _______,_______,_______,_______,_______,_______,                                       _______,KC_EXLM,KC_AT  ,KC_LCBR,KC_RCBR,KC_PIPE,
         _______,_______,_______,_______,_______,_______,                                       _______,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,
-        _______,_______,_______,_______,_______,_______,                                        _______,KC_HASH,KC_DLR ,KC_LPRN,KC_RPRN,KC_GRV ,
+        _______,_______,_______,_______,_______,_______,                                       _______,KC_HASH,KC_DLR ,KC_LPRN,KC_RPRN,KC_GRV ,
         _______,_______,_______,_______,_______,_______,_______,_______,       _______,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END ,_______,
                                                         _______,_______,       _______,_______                                             
     ),
@@ -119,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_LEVAN] = LAYOUT_6x6(
         _______,_______,_______,_______,_______,_______,                                       _______,_______,KC_PSLS,KC_PAST,KC_PMNS,_______,
-        _______,_______,_______,_______,_______,X(THORN_L,THORN_U),                            ______,KC_P7  ,KC_P8  ,KC_P9  ,KC_PPLS,_______,
+        _______,_______,_______,_______,_______,XP(THORN_L,THORN_U),                            _______,KC_P7  ,KC_P8  ,KC_P9  ,KC_PPLS,_______,
         _______,_______,_______,X(GRINF),X(GFSLGHT),X(BL_BRTG),                                _______,KC_P4  ,KC_P5  ,KC_P6  ,KC_PCMM,_______,
         _______,_______,_______,X(GFWINK),X(HORNS),_______,                                    _______,KC_P1  ,KC_P2  ,KC_P3  ,KC_PEQL,_______,
         _______,_______,_______,_______,_______,_______,_______,_______,       _______,_______,_______,_______,KC_P0 ,KC_DOT ,KC_PENT,_______, 
