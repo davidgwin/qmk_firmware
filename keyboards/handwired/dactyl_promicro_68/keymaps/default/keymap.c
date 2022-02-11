ea8d4edb1ef30f19cb5630b839fc638562038c20
David Gwin <davidgwin@ymail.com>
2022-02-11 12:58:47 -0700

    Removes dual function keys

--- keyboards/handwired/dactyl_promicro_68/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_promicro_68/keymaps/default/keymap.c
@@ -2,26 +2,27 @@
 
 
 #define _QWERTY 0
-#define _SALEM 1
-#define _MONA 2
-#define _LEVAN 3 
-#define _BEAVER 4 
-#define _KANAB 5 
+#define _LPURPLE 1
+#define _LGREEN 2
+#define _LYELLOW 3 
+#define _LORANGE 4 
+#define _LCYAN 5 
  
-#define MONA MO(_MONA)
-#define SALEM MO(_SALEM)
-#define LEVAN MO(_LEVAN) 
-#define BEAVER MO(_BEAVER) 
+#define LGREEN MO(_LGREEN)
+#define LPURPLE MO(_LPURPLE)
+#define LYELLOW MO(_LYELLOW) 
+#define LORANGE MO(_LORANGE) 
+#define LCYAN MO(_LCYAN) 
 
 #define LG_QUOT LGUI_T(KC_QUOT)
 #define RG_CAPS RGUI_T(KC_CAPS)
 #define CTL_ESC LCTL_T(KC_ESC)
 #define CTL_SPC LCTL_T(KC_SPC)
 #define CTR_SPC RCTL_T(KC_SPC)
-#define LVN_ESC LT(_LEVAN, KC_ESC) 
-#define DBEAVER LT(_BEAVER, KC_D)
-#define KKANAB LT(_KANAB, KC_K)
-#define FSALEM LT(_SALEM, KC_F)
+#define LVN_ESC LT(_LYELLOW, KC_ESC) 
+#define DLORANGE LT(_LORANGE, KC_D)
+#define KLCYAN LT(_LCYAN, KC_K)
+#define FLPURPLE LT(_LPURPLE, KC_F)
 #define ALT_RGT RALT(KC_RGHT)
 #define A_SHFT RSFT_T(KC_A)
 #define CL_SHFT RSFT_T(KC_SCLN)
@@ -137,21 +138,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_6x6(
         KC_GRAVE, KC_1 ,KC_2   , KC_3 , KC_4  , KC_5  ,                                        KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
         KC_TAB , KC_Q  ,KC_W   , KC_E , KC_R  , KC_T  ,                                        KC_Y   , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL ,
-        KC_CAPS, A_SHFT, KC_S ,DBEAVER, FSALEM , KC_G   ,                                        KC_H   , KC_J  ,KKANAB , KC_L  ,CL_SHFT,KC_QUOT,
+        KC_CAPS, KC_A  , KC_S   ,KC_D , KC_F , KC_G   ,                                        KC_H   , KC_J  ,KC_K , KC_L  ,KC_SCLN,KC_QUOT,
         KC_LSFT, KC_Z  ,KC_X   , KC_C , KC_V  ,KC_B  ,                                         KC_N   , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_SFTENT,
-        KC_LCTL,KC_LGUI,KC_LALT,KC_ESC ,KC_DEL,SALEM,KC_LSFT,KC_LCTL,       KC_RALT, KC_SPC, KC_RSFT, KC_LEFT,KC_DOWN, KC_UP , KC_RGHT,  KC_RCTL,                                                                       
-                                                        MONA ,LEVAN ,        KC_BSPC, KC_ENT
+        KC_LCTL,KC_LGUI,KC_LALT,KC_ESC ,KC_DEL,LPURPLE,KC_LSFT,KC_LCTL,       LORANGE, KC_SPC, LCYAN, KC_LEFT,KC_DOWN, KC_UP , KC_RGHT,  KC_RCTL,                                                                       
+                                                        LGREEN ,LYELLOW ,        KC_BSPC, KC_ENT
                                                                             
     ), 
-    [_SALEM] = LAYOUT_6x6(
+    [_LPURPLE] = LAYOUT_6x6(
         KC_F12 ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                                       KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
         _______,_______,_______,_______,_______,_______,                                       _______,KC_EXLM,KC_AT  ,KC_LCBR,KC_RCBR,KC_PIPE,
-        _______,_______,_______,_______,_______,_______,                                       _______,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,
-        _______,_______,_______,_______,_______,_______,                                       _______,KC_HASH,KC_DLR ,KC_LPRN,KC_RPRN,KC_GRV ,
+        _______,_______,_______,_______,_______,_______,                                       _______,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_BSLS,
+        _______,_______,_______,_______,_______,_______,                                       _______,KC_HASH,KC_DLR ,KC_LPRN,KC_RPRN,KC_TILD,
         _______,_______,_______,_______,_______,_______,_______,_______,       _______,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END ,_______,
                                                         _______,_______,       _______,_______                                             
     ),
-    [_MONA] = LAYOUT_6x6(
+    [_LGREEN] = LAYOUT_6x6(
         _______,RGB_HUI,_______,_______,_______,_______,                                       _______,_______,_______,_______,_______,_______,
         RGB_MOD,RGB_SAI,_______,_______,_______,_______,                                       _______,_______,KC_MS_U,_______,_______,_______,
         RGB_VAI  ,RGB_VAD ,KC_BTN3,KC_BTN2,KC_BTN1,_______,                                       _______,KC_MS_L,KC_MS_D,KC_MS_R,_______,_______,
@@ -160,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       _______,_______,        _______,KC_WH_D
                                                                               
     ),
-    [_LEVAN] = LAYOUT_6x6(
+    [_LYELLOW] = LAYOUT_6x6(
         _______,_______,_______,_______,_______,_______,                                       _______,_______,KC_PSLS,KC_PAST,KC_PMNS,_______,
         _______,_______,_______,_______,_______,XP(THORN_L,THORN_U),                            _______,KC_P7  ,KC_P8  ,KC_P9  ,KC_PPLS,_______,
         _______,_______,_______,X(GRINF),X(GFSLGHT),X(BL_BRTG),                                _______,KC_P4  ,KC_P5  ,KC_P6  ,KC_PCMM,_______,
@@ -169,7 +170,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         _______,_______,       _______,_______
                                                                                
     ),
-    [_BEAVER] = LAYOUT_6x6(
+    [_LORANGE] = LAYOUT_6x6(
         _______,_______,_______,_______,_______,_______,                                       _______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,                                       _______,_______,KC_UP  ,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,                                       _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,
@@ -177,7 +178,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,_______,_______,_______,_______,_______,       _______,_______,_______,_______,_______,_______,_______,_______,
                                                         _______,_______,       _______,_______   
     ),
-    [_KANAB] = LAYOUT_6x6(
+    [_LCYAN] = LAYOUT_6x6(
         _______,_______,_______,_______,_______,_______,                                       _______,_______,_______,_______,_______,_______,
         _______,_______,DM_RSTP,DM_PLY1,DM_PLY2,_______,                                       _______,_______,_______,_______,_______,_______,
         _______,_______,DM_RSTP,DM_REC1,DM_REC2,_______,                                       _______,_______,_______,_______,_______,_______,
@@ -200,10 +201,10 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(2, layer_state_cmp(state, _SALEM));
-    rgblight_set_layer_state(3, layer_state_cmp(state, _MONA));
-    rgblight_set_layer_state(4, layer_state_cmp(state, _LEVAN));
-    rgblight_set_layer_state(5, layer_state_cmp(state, _BEAVER));
-    rgblight_set_layer_state(6, layer_state_cmp(state, _KANAB));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _LPURPLE));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _LGREEN));
+    rgblight_set_layer_state(4, layer_state_cmp(state, _LYELLOW));
+    rgblight_set_layer_state(5, layer_state_cmp(state, _LORANGE));
+    rgblight_set_layer_state(6, layer_state_cmp(state, _LCYAN));
     return state;
 }
