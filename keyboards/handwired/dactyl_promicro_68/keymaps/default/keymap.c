b9f74165d598be97633281a983f89ad7835f3999
David Gwin <davidgwin@ymail.com>
2022-02-25 07:22:03 -0700

    adds ctrl-shift key, removes shft/enter and right/alt

--- keyboards/handwired/dactyl_promicro_68/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_promicro_68/keymaps/default/keymap.c
@@ -110,10 +110,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_6x6(
         KC_GRAVE, KC_1 , KC_2  , KC_3 , KC_4  , KC_5  ,                                        KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
         KC_TAB , KC_Q  , KC_W  , KC_E , KC_R  , KC_T  ,                                        KC_Y   , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL ,
-        KC_CAPS, KC_A  , KC_S   ,KC_D , KC_F  , KC_G  ,                                        KC_H   , KC_J  ,KC_K   , KC_L  ,KC_SCLN,KC_QUOT,
-        KC_LSFT, KC_Z  , KC_X  , KC_C , KC_V  , KC_B  ,                                        KC_N   , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_SFTENT,
-        KC_LCTL,KC_LGUI,KC_LALT,KC_ESC ,KC_DEL,LPURPLE,STL_SPC,CTL_ENT,       LCYAN , KC_SPC,  LAZURE ,KC_LEFT,KC_DOWN, KC_UP , ALT_RGT, KC_RCTL,                                                                       
-                                                        LGREEN,LYELLOW,       KC_BSPC, KC_TAB 
+        KC_CAPS, KC_A  , KC_S   ,KC_D , KC_F  , KC_G  ,                                        KC_H   , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+        KC_LSFT, KC_Z  , KC_X  , KC_C , KC_V  , KC_B  ,                                        KC_N   , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
+        KC_LCTL,KC_LGUI,KC_LALT,RCS(KC_ESC) ,KC_DEL,LPURPLE,STL_SPC,CTL_ENT,   LCYAN , KC_SPC, LAZURE ,KC_LEFT,KC_DOWN, KC_UP ,KC_RIGHT,KC_RCTL,                                                                       
+                                                        LGREEN,LYELLOW,       KC_BSPC, RALT_T(KC_TAB) 
                                                                         
     ), 
     [_LPURPLE] = LAYOUT_6x6(
