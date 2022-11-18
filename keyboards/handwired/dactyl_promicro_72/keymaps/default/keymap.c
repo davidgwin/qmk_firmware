a02d58e573f5f0b8f22f467dd7c697b9f7880587
David <davidgwin@outlook.com>
2022-11-17 22:32:06 -0700

    fix indentation and add f13 - f24 keys

--- keyboards/handwired/dactyl_promicro_72/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_promicro_72/keymaps/default/keymap.c
@@ -153,7 +153,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_6x6(
         KC_GRAVE, KC_1 , KC_2  , KC_3 , KC_4  , KC_5  ,                                                              KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
         KC_TAB , KC_Q  , KC_W  , KC_E , KC_R  , KC_T  ,                                                              KC_Y   , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL ,
- 
         AZ_CAPS, KC_A  , KC_S   ,KC_D , KC_F  , KC_G  ,                                                              KC_H   , KC_J  , KC_K  , KC_L  ,KC_SCLN,CY_QUOT,
         KC_LSFT, KC_Z  , KC_X  , KC_C , KC_V  , KC_B  ,                                                              KC_N   , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
         CTL_ENT,KC_LGUI,KC_LALT,CTSHESC,LAZURE,PRPL_BSP, KC_SPC,KC_LSFT,KC_LCTL,            KC_RCTL  ,KC_RSFT, KC_SPC,PRPL_BSP,LCYAN ,KC_LBRC, KC_RBRC ,KC_BSLS,CTR_ENT,
@@ -186,7 +185,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         _______,_______,_______,              _______,_______,_______
     ),
     [_LAZURE] = LAYOUT_6x6(
-        _______,_______,_______,_______,_______,_______,                                                              _______,_______,_______,_______,_______,_______,
+        KC_F24 ,KC_F13 ,KC_F14 ,KC_F15 ,KC_F16 ,KC_F17 ,                                                              KC_F18 ,KC_F19 ,KC_F20 ,KC_F21 ,KC_F22 ,KC_F23 ,
         _______,_______,_______,_______,_______,_______,                                                              _______,C_UPDAT,_______,_______,KC_PSCR,KC_PIPE,
         _______,_______,_______,_______,C_FLASH,_______,                                                              KC_HOME,KC_PGDN,KC_PGUP,KC_END ,_______,KC_BSLS,
         _______,_______,_______,C_COMPI,_______,_______,                                                              _______,_______,_______,_______,_______,_______,
