90026ce9b1fcab870cb0a33fb55d1ec1fddad3ff
David <davidgwin@outlook.com>
2022-11-26 09:45:09 -0700

    change purple-backspace to purple-delete

--- keyboards/crynu/keymaps/default/keymap.c
+++ keyboards/crynu/keymaps/default/keymap.c
@@ -115,11 +115,11 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY]  = LAYOUT(/* qwerty */
-        KC_GRV, KC_1,  KC_2,   KC_3,   KC_4,   KC_5,                                                         KC_6,    KC_7,  KC_8,   KC_9,   KC_0,   KC_MINS, 
+        KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                                                        KC_6,    KC_7,  KC_8,   KC_9,   KC_0,   KC_MINS, 
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                                        KC_Y,    KC_U,  KC_I,   KC_O,   KC_P,   KC_EQL ,
         STL_CAP,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                                        KC_H,    KC_J,  KC_K,   KC_L,   KC_SCLN,KC_QUOT,
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                                                        KC_N,    KC_M,  KC_COMM,KC_DOT, KC_SLSH,KC_RSFT, 
-        CTL_ENT,KC_LGUI,KC_LALT,LVN_ESC,LGREEN, PRPL_BSP, KC_SPC, D_CYAN,                    LAZURE, KC_SPC, PRPL_BSP,LGREEN,KC_LBRC,KC_RBRC,KC_BSLS,CTR_ENT
+        CTL_ENT,KC_LGUI,KC_LALT,LVN_ESC,LGREEN, PRPL_DEL, KC_SPC, LCYAN,                    LAZURE, KC_SPC, PRPL_BSP,LGREEN,KC_LBRC,KC_RBRC,KC_BSLS,CTR_ENT
     ),
     
     [_COLMAK]  = LAYOUT(
