077849a8d3da38288cb2a0ba65e91c5f5450afe6
David Gwin <davidgwin@ymail.com>
2022-12-15 00:29:39 -0700

    adds mad and crynu updates

--- keyboards/crynu/keymaps/default/keymap.c
+++ keyboards/crynu/keymaps/default/keymap.c
@@ -29,7 +29,7 @@
 #define LCYAN MO(_LCYAN)
 #define LSYSL MO(_LSYSL) 
 
-#define LG_QUOT LGUI_T(KC_QUOT)
+#define CY_QUOT LT(_LCYAN, KC_QUOT)
 #define RG_CAPS RGUI_T(KC_CAPS)
 #define CTL_ENT LCTL_T(KC_ENT)
 #define CTR_ENT RCTL_T(KC_ENT)
@@ -38,6 +38,7 @@
 #define STL_SPC LSFT_T(KC_SPC)
 #define STL_CAP LSFT_T(KC_CAPS)
 #define STR_SPC RSFT_T(KC_SPC)
+#define LVN_ENT LT(_LYELLOW, KC_ENT)
 #define LVN_ESC LT(_LYELLOW, KC_ESC)
 #define D_AZURE LT(_LAZURE, KC_DEL)
 #define E_AZURE LT(_LAZURE, KC_ENT)
@@ -117,9 +118,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY]  = LAYOUT(/* qwerty */
         KC_GRV, KC_1,  KC_2,   KC_3,   KC_4,   KC_5,                                                         KC_6,    KC_7,  KC_8,   KC_9,   KC_0,   KC_MINS, 
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                                        KC_Y,    KC_U,  KC_I,   KC_O,   KC_P,   KC_EQL ,
-        STL_CAP,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                                        KC_H,    KC_J,  KC_K,   KC_L,   KC_SCLN,KC_QUOT,
+        AZ_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                                        KC_H,    KC_J,  KC_K,   KC_L,   KC_SCLN,CY_QUOT,
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                                                        KC_N,    KC_M,  KC_COMM,KC_DOT, KC_SLSH,KC_RSFT, 
-        CTL_ENT,KC_LGUI,KC_LALT,LVN_ESC,LGREEN, PRPL_BSP, KC_SPC, D_CYAN,                    LAZURE, KC_SPC, PRPL_BSP,LGREEN,KC_LBRC,KC_RBRC,KC_BSLS,CTR_ENT
+        CTL_ENT,KC_LGUI,KC_LALT,LVN_ESC,LGREEN, PRPL_BSP, LVN_ENT, D_CYAN,                    LAZURE, KC_SPC, PRPL_BSP,LGREEN,KC_LBRC,KC_RBRC,KC_BSLS,CTR_ENT
     ),
     
     [_COLMAK]  = LAYOUT(
---
+++ keyboards/mad/config.h
@@ -0,0 +1,41 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "config_common.h"
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 3
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { D7, C6, B6, D4 }
+#define MATRIX_COL_PINS { B5, B4, E6 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION ROW2COL
+
+/* Bootmagic key configuration */
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 2
+
+
+#define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_1X
+#define CIRQUE_PINNACLE_CURVED_OVERLAY
+// #define CIRQUE_PINNACLE_POSITION_MODE CIRQUE_PINNACLE_ABSOLUTE_MODE
+
+#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
+#define CIRQUE_PINNACLE_TAP_ENABLE
